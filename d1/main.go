7ec0be622aed1abe6ab411515e300a24666d8c56
ikiris <4852950+ikiris@users.noreply.github.com>
2023-12-01 21:19:46 -0600

    fix original p1 test

--- d1/main.go
+++ d1/main.go
@@ -13,24 +13,15 @@ type tracker struct {
 }
 
 var listA = map[string]int{
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
-	"1":     1,
-	"2":     2,
-	"3":     3,
-	"4":     4,
-	"5":     5,
-	"6":     6,
-	"7":     7,
-	"8":     8,
-	"9":     9,
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
 }
 
 var listB = map[string]int{
